671a41e5366c9d22a84509084cb0fce4460abad1
ImNuro <56514787+ImNuro@users.noreply.github.com>
2021-04-10 01:17:45 +0200

    k

---
+++ src/main/java/utils/NewPostEvent.kt
@@ -0,0 +1,18 @@
+package utils
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class NewPostEvent {
+    fun onNewPost() {
+        val client = HttpClient.newBuilder().build();
+        val request = HttpRequest.newBuilder()
+                .uri(URI.create("http://reddit.boese.ovh"))
+                .build()
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        println(response.body())
+    }
+}
